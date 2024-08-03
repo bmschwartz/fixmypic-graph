3b6460adb3ed7682aa6647334cac4687c0146d4a
Ben Schwartz <schwartz.ben0@gmail.com>
2024-08-03 17:33:37 -0400

    Lots of changes

--- src/mappings/requestSubmissionMapper.ts
+++ src/mappings/requestSubmissionMapper.ts
@@ -4,11 +4,13 @@ import { SubmissionPurchase } from "../../generated/schema";
 export function handleSubmissionPurchased(event: SubmissionPurchased): void {
   const purchaser = event.params.purchaser;
   const submission = event.params.submission;
+  const price = event.params.price;
   const purchaseId = `${purchaser.toHex()}-${submission.toHex()}`;
 
   let purchase = new SubmissionPurchase(purchaseId);
   purchase.submission = submission.toHex();
   purchase.purchaser = purchaser;
+  purchase.price = price;
   purchase.purchaseDate = event.params.purchaseDate;
   purchase.blockNumber = event.block.number;
   purchase.blockTimestamp = event.block.timestamp;
